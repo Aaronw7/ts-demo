e29ad076cea9d6e75f4452869138cd215e70c3e1
AaronW <aaronwang96@hotmail.com>
2023-07-06 04:51:16 -0700

    create routes and Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,27 @@
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/profile" element={<div>Profile</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ frontend/src/scenes/navbar/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import SchoolIcon from '@mui/icons-material/School';
+import { Box, Typography, useTheme } from '@mui/material';
+
+const Navbar = () => {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      mb="0.25rem"
+      p="0.5rem 0rem"
+      color={palette.grey[300]}
+    >
+      {/* Left Side */}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        gap="0.75rem"
+      >
+        <SchoolIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          TS Demo
+        </Typography>
+      </Box>
+      {/* Right Side */}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box sx={{ "&:hover": {
+          color: palette.primary[100]
+        }}}>
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+              margin: "0rem 1rem"
+            }}
+          >
+            Dashboard
+          </Link>
+          <Link
+            to="/profile"
+            onClick={() => setSelected("profile")}
+            style={{
+              color: selected === "profile" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            Profile
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default Navbar;
