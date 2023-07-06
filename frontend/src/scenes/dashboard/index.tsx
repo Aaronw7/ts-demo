b3d755d2fc77d92dd3b0c0766dbf025a4f8f0f0a
AaronW <aaronwang96@hotmail.com>
2023-07-06 06:09:03 -0700

    setup initial architecture and api

---
+++ frontend/src/scenes/dashboard/index.tsx
@@ -0,0 +1,71 @@
+import {Box, useMediaQuery } from '@mui/material';
+import DashboardBox from '@/components/DashboardBox';
+
+const Dashboard = () => {
+  const isAboveSmallScreens = useMediaQuery("(min-width: 1200px");
+
+  const gridTemplateLarge = `
+  "a a b"
+  "a a b"
+  "a a b"
+  "a a c"
+  "a a c"
+  "a a c"
+  "a a d"
+  "f f d"
+  "f f e"
+  "f f e"
+  `;
+
+  const gridTemplateSmall = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  `
+
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx= {
+        isAboveSmallScreens ? {
+          gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+          gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+          gridTemplateAreas: gridTemplateLarge,
+        } : {
+          gridAutoColumns: "1fr",
+          gridAutoRows: "80px",
+          gridTemplateAreas: gridTemplateSmall,
+        }
+      }
+    >
+      <DashboardBox gridArea="a"></DashboardBox>
+      <DashboardBox gridArea="b"></DashboardBox>
+      <DashboardBox gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="d"></DashboardBox>
+      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="f"></DashboardBox>
+    </Box>
+  )
+}
+
+export default Dashboard;
---
+++ frontend/src/state/api.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["PersonalInfo"],
+  endpoints: (build) => ({
+    getPersonalInfo: build.query({
+      query: () => "personalInfo/personalInfo/",
+      providesTags: ["PersonalInfo"]
+    })
+  })
+})
+
+export const { useGetPersonalInfoQuery } = api;
